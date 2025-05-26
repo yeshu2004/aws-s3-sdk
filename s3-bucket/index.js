91d79273d792c9b4b2efd5b4e236292c5b395753
yeshu2004 <playeryeshu@gmail.com>
2025-05-26 20:03:25 +0530

    crud added

--- s3-bucket/index.js
+++ s3-bucket/index.js
@@ -1,4 +1,4 @@
-const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, GetObjectCommand, PutObjectCommand, ListBucketsCommand, ListObjectsCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const { configDotenv } = require("dotenv");
 const {readFile} = require('fs/promises')
@@ -38,8 +38,48 @@ async function putBucketObj(key, filePath) {
     console.log(`Uploaded ${key} with Content-Type: ${contentType}`);
 }
 
+// list all buckets...
+async function listAllBucket() {
+    const listCommand = new ListBucketsCommand({
+        BucketRegion: process.env.BucketRegion,
+    })
+    
+    const response = await client.send(listCommand)
+    console.log(response) 
+}
+
+// list all obj from a bucket...
+async function listAllObject(params) {
+    const listObjectsCommand = new ListObjectsCommand({
+        Bucket: process.env.BUCKET_NAME,
+    })
+    
+    const res = await client.send(listObjectsCommand)
+    console.log(res);
+}
+
+// delete obj from a bucket...
+async function deleteOject(key) {
+    const command = new DeleteObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: key,
+    })
+
+    const res = await client.send(command)
+    return res;
+}
+
+
+await 
+
+
+
 async function init(){
-    await putBucketObj("uploadImg.jpg", "../img/uploadImg.jpg");
+    // await putBucketObj("uploadImg.jpg", "../img/uploadImg.jpg");
     // console.log('URL for imgone(Object)', await getBucketObjURL("imgg.png"))
+    // await listAllBucket();
+    await listAllObject(); 
+    // await deleteOject("imgone")
+
 }
 init()
